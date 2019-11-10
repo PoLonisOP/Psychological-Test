99beb04a7756bef87618d14497b3384abdaafafa
tony <qq1456978523@gmail.com>
2019-11-10 10:59:32 +0800

    Initial Commit

---
+++ New_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New_App
+//
+//  Created by User16 on 2019/11/10.
+//  Copyright © 2019 00657018. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
